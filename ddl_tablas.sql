74ae45a9cff4f180ce0618912f583ec9fa7793ff
Ademir <ademirjh1@gmail.com>
2019-10-26 11:45:35 -0500

    CUARTO CAMBIO

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,11 +1,11 @@
-creacion de tablas SQL
-Creador: Ademir Jáuregui
-
+-- creacion de tablas SQL
+-- Creador: Ademir Jáuregui
+-- trabajando en rama Alterna : "cuarto cambio --> en alterna"
 
 create table (
 nombre prueba3 (50)
 apellido prueba3 (50)
 )
-
+tablespace tmp; 
 
 
